c1aee8eb26a0e2990a437f0ff8ee4db433fa8171
erianvc <erianvc.dev@gmail.com>
2022-06-14 20:13:23 -0500

    feat: adds Text component

---
+++ src/components/atoms/Text.svelte
@@ -0,0 +1,18 @@
+<script>
+  /** @type {'h4' | 'h5' | 'h6' | 'p' | 'span'} */
+  export let tag
+  /** @type {boolean} */
+  export let opaque = false
+  /** @type {boolean} */
+  export let large = false
+  /** @type {boolean} */
+  export let small = false
+  /** @type {boolean} */
+  export let tiny = false
+
+  const text = true
+</script>
+
+<svelte:element this={tag} {...$$restProps} class:text class:opaque class:tiny class:small class:large>
+  <slot />
+</svelte:element>
