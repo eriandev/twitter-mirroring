370a7e70c861ac480c8e10fab8e41f48c50ca260
erianvc <erianvc.dev@gmail.com>
2022-06-14 20:56:27 -0500

    feat: adds Link component

---
+++ src/components/atoms/Link.svelte
@@ -0,0 +1,22 @@
+<script>
+  /** @type {boolean} */
+  export let external = false
+  /** @type {boolean} */
+  export let primary = false
+  /** @type {boolean} */
+  export let decorated = false
+  /** @type {boolean} */
+  export let tiny = false
+  /** @type {string} */
+  export { href as to }
+
+  /** @type {string} */
+  let href
+
+  const target = external ? '_blank' : undefined
+  const rel = external ? 'external noopener noreferrer' : undefined
+</script>
+
+<a {...$$restProps} class:tiny class:primary class:decorated {rel} {target} {href}>
+  <slot />
+</a>
