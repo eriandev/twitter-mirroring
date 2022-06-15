3824775f7b38fb5ca62fd80f8d223f909016850d
erianvc <erianvc.dev@gmail.com>
2022-06-14 21:12:47 -0500

    feat: adss Icon component

---
+++ src/components/atoms/Icon.svelte
@@ -0,0 +1,16 @@
+<script>
+  // @ts-ignore
+  import InlineSVG from 'svelte-inline-svg'
+
+  /** @type {string} */
+  export let name
+  /** @type {number} */
+  export let size = 32
+
+  $: sizing = {
+    width: size,
+    height: size,
+  }
+</script>
+
+<InlineSVG src="icons/{name}.svg" {name} {...sizing} {...$$restProps} on:click on:mouseenter on:mouseleave />
