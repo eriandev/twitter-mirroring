e1bef0fc09e0a0fb11b21b283b169fb83b30af8c
erianvc <erianvc.dev@gmail.com>
2022-06-14 20:49:00 -0500

    feat: adds Button component

---
+++ src/components/atoms/Button.svelte
@@ -0,0 +1,18 @@
+<script>
+  /** @type {boolean} */
+  export let primary = false
+  /** @type {boolean} */
+  export let secondary = false
+  /** @type {boolean} */
+  export let ghost = false
+  /** @type {boolean} */
+  export let danger = false
+  /** @type {boolean} */
+  export let large = false
+
+  const btn = true
+</script>
+
+<button {...$$restProps} class:btn class:primary class:secondary class:ghost class:danger class:large on:click on:mouseenter on:mouseleave>
+  <slot />
+</button>
