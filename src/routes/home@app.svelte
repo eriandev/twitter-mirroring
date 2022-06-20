dc3b168556b5598c56d5a66d35062704eecd5042
erianvc <erianvc.dev@gmail.com>
2022-06-20 16:00:41 -0500

    fix: corrects home page styles

---
+++ src/routes/home@app.svelte
@@ -0,0 +1,7 @@
+<script>
+  // @ts-nocheck
+  import Tweets from '@/components/organisms/Tweets.svelte'
+  import eriandevTweets from '@/assets/data/eriandev_tweets.json'
+</script>
+
+<Tweets tweets={eriandevTweets} />
